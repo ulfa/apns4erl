ca82d9d5bd37783a0db2acd076147c238f7ec068
ua <ulf.angermann@innoq.com>
2016-04-22 09:33:23 +0200

    changed the code which uses jiffy

--- src/apns_connection.erl
+++ src/apns_connection.erl
@@ -325,8 +325,8 @@ code_change(_OldVsn, State, _Extra) ->  {ok, State}.
 %% Private functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 build_payload(Params, Extra, Content_Available) ->
-  jsx:encode(
-    {[{<<"aps">>, do_build_payload(Params, Content_Available)} | Extra]}).
+  {P} = do_build_payload(Params, Content_Available),
+  jsx:encode([{<<"aps">>, P} | Extra]).
 
 do_build_payload(Params, Content_Available) when Content_Available ->
   do_build_payload(Params, [{<<"content-available">>, 1}]);
